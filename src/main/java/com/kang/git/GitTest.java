3ba466b5980b4a4dbbe1c64ed47d61bd243b02cb
fish <37338557+IBraveheart@users.noreply.github.com>
2021-09-02 21:42:28 +0800

    pull commit

--- src/main/java/com/kang/git/GitTest.java
+++ src/main/java/com/kang/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master merge commit");
         System.out.println("hot-fix commit merge");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
