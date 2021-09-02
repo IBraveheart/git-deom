f92a9ed156f06ee1f33e7c04fb17e42bd42ad4cc
fish <fish@kang.com>
2021-09-02 20:59:03 +0800

    first commit

---
+++ src/main/java/com/kang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.kang.git;
+
+/**
+ * @author smallfish
+ * @create 2021-09-02 20:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
